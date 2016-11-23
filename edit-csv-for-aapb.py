fb8432b14ae7ab33f07e52a0dc30cb36c4b8d1d3
selenachau <selenachau@gmail.com>
2016-11-22 18:51:46 -0800

    added test python
    column headers don't match current database column headers

---
+++ edit-csv-for-aapb.py
@@ -0,0 +1,16 @@
+import pandas as pd
+pd.options.mode.chained_assignment = None  # default='warn'
+df = pd.read_csv("kboo_pbcore_master.csv")
+
+keep_cols = ["assetIdentifier", "physicalFormat", "Generations", "DurationPhysical", "originalCarrierLocation","Date", "Date Type","itemTitle", "intellectualAssetType"]
+
+new_df = df[keep_cols]
+new_df.rename(columns={'assetIdentifier': 'Unique Identifier', 'physicalFormat': 'Physical Format',
+'DurationPhysical':"Duration","originalCarrierLocation":'Location', 'itemTitle':'Title','intellectualAssetType':'Title Type'}, inplace=True)
+new_df["Identifier Source"] = "KBOO item label"
+new_df["Media Type"] = "Sound"
+new_df["Digital Format"] = "audio/x-wav"
+new_df["Duration Approximate"] = "Yes"
+new_df = new_df[['Unique Identifier', 'Identifier Source', 'Physical Format', 'Digital Format', 'Generations','Duration','Location','Date','Date Type','Title','Title Type']]
+
+new_df.to_csv("aapb-test-data.csv", index=False)
