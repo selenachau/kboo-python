7fe7b57433a387189a4f3816e01e4793c296166e
selenachau <selenachau@gmail.com>
2016-11-28 10:16:22 -0800

    for kboo test server rs data

--- edit-csv-for-aapb.py
+++ edit-csv-for-aapb.py
@@ -1,16 +1,16 @@
 import pandas as pd
 pd.options.mode.chained_assignment = None  # default='warn'
-df = pd.read_csv("https://github.com/selenachau/kboo-metadata/blob/master/search_results_metadata.csv")
+df = pd.read_csv("search_results_metadata_import.csv")
 
 keep_cols = ["assetIdentifier", "physicalFormat", "generations", "durationPhysical", "originalCarrierLocation","date", "dateType","itemTitle", "intellectualAssetType"]
 
 new_df = df[keep_cols]
-new_df.rename(columns={'assetIdentifier': 'Unique Identifier', 'physicalFormat': 'Physical Format',
-'DurationPhysical':"Duration","originalCarrierLocation":'Location', 'itemTitle':'Title','intellectualAssetType':'Title Type'}, inplace=True)
+new_df.rename(columns={'assetIdentifier': 'Unique Identifier', 'physicalFormat': 'Physical Format', 'generations': 'Generations', 'date': 'Date', 'dateType': 'Date Type',
+'durationPhysical':"Duration","originalCarrierLocation":'Location', 'itemTitle':'Title','intellectualAssetType':'Title Type'}, inplace=True)
 new_df["Identifier Source"] = "KBOO item label"
 new_df["Media Type"] = "Sound"
 new_df["Digital Format"] = "audio/x-wav"
 new_df["Duration Approximate"] = "Yes"
 new_df = new_df[['Unique Identifier', 'Identifier Source', 'Physical Format', 'Digital Format', 'Generations','Duration','Location','Date','Date Type','Title','Title Type']]
 
-new_df.to_csv("aapb-test-data.csv", index=False)
+new_df.to_csv("aapb-test-data-export.csv", index=False)
